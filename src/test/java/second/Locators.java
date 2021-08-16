accdfe4fad995cc57847f33992ecf123d9b141e9
New Sheriff in Town <dimashedin51@gmail.com>
2021-08-16 11:45:45 +0300

    first selenium test with TestNG

--- src/test/java/second/Locators.java
+++ src/test/java/second/Locators.java
@@ -1,6 +1,5 @@
 package second;
 
-import first.CustomConditions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
---
+++ src/test/java/third/TestNG.java
@@ -0,0 +1,47 @@
+package third;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class TestNG {
+    private WebDriver driver;
+
+    @BeforeMethod(alwaysRun = true) // перед каждым методом открываем новый браузер
+    public void browserSetup(){
+        System.setProperty("webdriver.chrome.driver", "D:\\selenium drivers\\bin\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        driver = new ChromeDriver(options);
+        driver.get("https://football24.ua/");
+    }
+
+    @Test(description = "Count of transfers on the Transfers page")
+    public void transfersPageHasResults(){
+        WebElement transfersButton = waitForElementLocatedBy(driver, By.xpath("//li[@id='menu-id-384']/a"));
+        transfersButton.click();
+        List<WebElement> countOfTransfers = driver.findElements(By.xpath("//div[@id='aazone.transfersZone']/div[@class='small-12 grid-container transfers-card']"));
+        Assert.assertEquals(countOfTransfers.size(),10,"Found no 10 elements!");
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void closeBrowser() {
+        driver.quit();
+        driver=null;
+    }
+
+    private static WebElement waitForElementLocatedBy(WebDriver driver, By by) {
+        return new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+}
